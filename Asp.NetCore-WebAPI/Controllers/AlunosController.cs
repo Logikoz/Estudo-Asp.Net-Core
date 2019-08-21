8cf15e6ba3aa7a21f0be03bd1d4e1c593a15f0b2
Logikoz <37851168+LogikozRC@users.noreply.github.com>
2019-08-21 10:40:31 -0500

    Adicionar arquivos de projeto.

---
+++ Asp.NetCore-WebAPI/Controllers/AlunosController.cs
@@ -0,0 +1,35 @@
+﻿using Asp.NetCore_WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Asp.NetCore_WebAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    public class AlunosController : Controller
+    {
+        private static readonly List<Aluno> _alunos = new List<Aluno>();
+        [HttpGet]
+        public IActionResult MostrarAlunos()
+        {
+            return Ok(_alunos);
+        }
+        [HttpPost("add/{nome}/{nota}/{dataNascimento}/{dataTermino?}")]
+        public void AdicionarAluno(string nome, float nota, DateTime dataNascimento, DateTime? dataTermino)
+        {
+            _alunos.Add(new Aluno
+            {
+                Nome = nome,
+                NotaMedia = nota,
+                DataNascimento = dataNascimento,
+                DataTermino = dataTermino
+            });
+        }
+        [HttpDelete("delete/{id}")]
+        public void RemoverAluno(Guid id)
+        {
+            _alunos.RemoveAt(_alunos.IndexOf(_alunos.First(x => x.Id.Equals(id))));
+        }
+    }
+}
---
+++ Asp.NetCore-WebAPI/Models/Aluno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Asp.NetCore_WebAPI.Models
+{
+    public class Aluno
+    {
+        public Aluno()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; }
+        public String Nome { get; set; }
+        public float NotaMedia { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime? DataTermino { get; set; }
+    }
+}
