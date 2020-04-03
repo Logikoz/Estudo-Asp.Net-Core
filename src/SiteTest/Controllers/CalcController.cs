f9d0c2af3a7f77bfebe4006798f2cde056c2185c
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-04-02 21:34:17 -0500

    ✨ Testando asp.net core 3.1

---
+++ src/SiteTest/Controllers/CalcController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test(int value) => Ok(value * 2);
+
+        [HttpGet("Red")]
+        public IActionResult Redirect() => RedirectToAction(nameof(HomeController.Privacy), GetController<HomeController>());
+
+        private string GetController<T>() where T : Controller => typeof(T).Name.Replace(nameof(Controller), string.Empty);
+    }
+}
